354af9acfcb96bce60044f0c3a1609cddfddb451
jurriaan <jurriaan@pruijs.nl>
2025-01-26 20:30:10 +0100

    fix trusted users

--- modules/nix.nix
+++ modules/nix.nix
@@ -10,6 +10,7 @@
         "https://helix.cachix.org/"
         "https://cache.garnix.io/"
       ];
+      trusted-users = [ "root" "jurriaan" ];
       trusted-public-keys = [
         "union.cachix.org-1:TV9o8jexzNVbM1VNBOq9fu8NK+hL6ZhOyOh0quATy+M="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
